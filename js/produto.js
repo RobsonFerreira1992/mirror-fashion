d63a3f46463d47527dbd8df85c6844ec9c01c475
Robson Miranda <robson1992ferreira@hotmail.com>
2020-03-24 09:54:16 -0300

    Mirror Fashion

---
+++ js/produto.js
@@ -0,0 +1,8 @@
+
+var inputTamanho = document.querySelector('input#tamanho');
+var outputTamanho = document.querySelector('output[for="tamanho"]');
+
+
+inputTamanho.oninput=function(){
+    outputTamanho.value = inputTamanho.value;
+}
